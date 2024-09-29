1bf236c338fbf038daa8680e67aeb32f8dbb0a0f
Argusto Lima <augusto.vlima2@gmai.com>
2024-09-28 23:56:05 -0300

    commit

---
+++ app/src/main/java/com/example/mobile_android_codesauro/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.mobile_android_codesauro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.mobile_android_codesauro.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val webView = binding.webView
+
+        // Configurações do WebView
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+        webView.settings.builtInZoomControls = false  // Desativar o zoom
+        webView.settings.displayZoomControls = false
+
+        // Adicionando uma meta tag para desativar o zoom no Android
+        webView.webChromeClient = WebChromeClient()
+        val metaTag = """
+            var meta = document.createElement('meta');
+            meta.name = 'viewport';
+            meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+            var head = document.getElementsByTagName('head')[0];
+            head.appendChild(meta);
+        """.trimIndent()
+        webView.evaluateJavascript(metaTag, null)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                return false
+            }
+        }
+
+        webView.loadUrl("http://10.0.2.2:4200/")
+    }
+}
